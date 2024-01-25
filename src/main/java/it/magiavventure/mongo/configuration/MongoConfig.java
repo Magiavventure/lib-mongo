9e757c820493078a758d8399b4950704c122dfbf
marco-polverini-dev <128422361+marco-polverini-dev@users.noreply.github.com>
2024-01-25 21:44:40 +0100

    fix: mongo config repo (#2)

--- src/main/java/it/magiavventure/mongo/configuration/MongoConfig.java
+++ src/main/java/it/magiavventure/mongo/configuration/MongoConfig.java
@@ -3,9 +3,11 @@ package it.magiavventure.mongo.configuration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
 @EnableMongoAuditing
+@EnableMongoRepositories("it.magiavventure.mongo.repository")
 @ComponentScan("it.magiavventure.mongo")
 public class MongoConfig {
 
