e7d340546481643fdaaa42d46e391ba9c74cd43e
Marco Polverini <marco.polverini.dev@gmail.com>
2024-02-05 22:50:14 +0100

    feat: added story entity/dto/repo

--- src/main/java/it/magiavventure/mongo/model/Story.java
+++ src/main/java/it/magiavventure/mongo/model/Story.java
@@ -1,6 +1,7 @@
 package it.magiavventure.mongo.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -8,6 +9,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -15,13 +17,18 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public class Category implements Serializable {
-
+public class Story implements Serializable {
     @NotNull
     private UUID id;
     @NotNull
-    private String name;
+    private String title;
+    private String subtitle;
     @NotNull
-    private String background;
-
+    private String text;
+    @NotNull
+    private String author;
+    @NotEmpty
+    private List<Category> categories;
+    @NotEmpty
+    private int readTime;
 }
